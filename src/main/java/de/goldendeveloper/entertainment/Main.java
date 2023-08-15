9124f27b99efa747dccb6f1c81ae9ade8c0feed1
c.ilgner <c.ilgner@tmg-web.de>
2023-08-15 11:44:08 +0200

    FIX: RegisterCommands

--- src/main/java/de/goldendeveloper/entertainment/Main.java
+++ src/main/java/de/goldendeveloper/entertainment/Main.java
@@ -2,17 +2,14 @@ package de.goldendeveloper.entertainment;
 
 import de.goldendeveloper.dcbcore.DCBot;
 import de.goldendeveloper.dcbcore.DCBotBuilder;
-import de.goldendeveloper.dcbcore.interfaces.CommandInterface;
 import de.goldendeveloper.entertainment.discord.CustomEvents;
 import de.goldendeveloper.entertainment.discord.commands.*;
 import de.goldendeveloper.entertainment.util.AudioPlayerHelper;
 
-import java.util.LinkedList;
-
 public class Main {
 
     private static MysqlConnection mysqlConnection;
-    private static  CustomConfig customConfig;
+    private static CustomConfig customConfig;
     private static DCBot dcBot;
     private static AudioPlayerHelper audioPlayerHelper;
 
@@ -20,27 +17,23 @@ public class Main {
     public static void main(String[] args) {
         customConfig = new CustomConfig();
         DCBotBuilder dcBotBuilder = new DCBotBuilder(args, true);
-        dcBotBuilder.registerCommands(registerCommands());
+        dcBotBuilder.registerCommands(
+                new CountingGame(),
+                new DeleteCountingGame(),
+                new Entertainment(),
+                new Pause(),
+                new Play(),
+                new Resume(),
+                new ScissorsRockPaper(),
+                new Skip(),
+                new Stop(),
+                new Volume(),
+                new YtSearch()
+        );
         dcBotBuilder.registerEvents(new CustomEvents());
         dcBot = dcBotBuilder.build();
         audioPlayerHelper = new AudioPlayerHelper();
         mysqlConnection = new MysqlConnection(customConfig.getMysqlHostname(), customConfig.getMysqlUsername(), customConfig.getMysqlPassword(), customConfig.getMysqlPort());
-    }
-
-    private static LinkedList<CommandInterface> registerCommands() {
-        LinkedList<CommandInterface>  commands = new LinkedList<>();
-        commands.add(new CountingGame());
-        commands.add(new DeleteCountingGame());
-        commands.add(new Entertainment());
-        commands.add(new Pause());
-        commands.add(new Play());
-        commands.add(new Resume());
-        commands.add(new ScissorsRockPaper());
-        commands.add(new Skip());
-        commands.add(new Stop());
-        commands.add(new Volume());
-        commands.add(new YtSearch());
-        return commands;
     }
 
     public static MysqlConnection getMysqlConnection() {
